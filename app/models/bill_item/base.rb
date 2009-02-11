aee022835634599e6a9fe4c3182da11218fcb57f
nay <nay@meadowy.org>
2009-02-11 17:37:11 +0900

    added formatted_amount

--- app/models/bill_item/base.rb
+++ app/models/bill_item/base.rb
@@ -2,13 +2,25 @@ class BillItem::Base < ActiveRecord::Base
   set_table_name "bill_items"
   belongs_to :bill
 
-  attr_protected :bill_id, :parent_id
-  attr_accessor :formatted_amount
+  attr_protected :bill_id, :parent_id, :amount
+  attr_accessor :formatted_amount, :set_formatted_amount
 
-  before_save :avoid_saving_abstract
+  before_save :avoid_saving_abstract, :set_amount
+  after_save :set_formatted_amount
+
+  def formatted_amount
+    set_formatted_amount if !@formatted_amount && self.amount
+    @formatted_amount
+  end
 
   private
   def avoid_saving_abstract
     raise "Can't save BillItem::Base object" if self.class == BillItem::Base
+  end
+  def set_amount
+    self.amount = self.formatted_amount ? self.formatted_amount.sub(/,/, '').to_i : nil
+  end
+  def set_formatted_amount
+    self.formatted_amount = amount ? ((amount.to_s =~ /[-+]?\d{4,}/) ? (amount.to_s.reverse.gsub(/\G((?:\d+\.)?\d{3})(?=\d)/, '\1,').reverse) : amount.to_s) : nil
   end
 end
--- spec/models/bill_item/base_spec.rb
+++ spec/models/bill_item/base_spec.rb
@@ -11,12 +11,22 @@ describe BillItem::Base do
       i = BillItem::Base.new(:parent_id => 5)
       i.parent_id.should be_nil
     end
+    it "amountは一括指定できない" do
+      i = BillItem::Base.new(:amount => 5)
+      i.amount.should be_nil
+    end
   end
 
   describe "save" do
     it "BillItem::Baseクラスのオブジェクトを保存しようとすると例外が発生する" do
       i = BillItem::Base.new
       lambda{i.save}.should raise_error(RuntimeError)
+    end
+    it "formatted_amount に,付きの数字を渡しても正しくamountに登録される" do
+      i = BillItem::Sale.new(:formatted_amount => "40,000")
+      i.save!
+      i.formatted_amount.should == "40,000"
+      i.amount.should == 40000
     end
   end
 
