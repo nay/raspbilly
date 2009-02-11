c29dbce24a5fc08444136f0a3eecd0b1aa9c4740
jugyo <jugyo.org@gmail.com>
2009-02-11 17:29:56 +0900

    add some specs

--- spec/models/numbering_rule_spec.rb
+++ spec/models/numbering_rule_spec.rb
@@ -9,7 +9,30 @@ describe NumberingRule do
     }
   end
 
-  it "should create a new instance given valid attributes" do
+  it "モデルをちゃんと作成できること" do
     NumberingRule.create!(@valid_attributes)
+  end
+
+  it "モデルの作成に失敗すること" do
+	lambda { NumberingRule.create! }.should raise_error(ActiveRecord::RecordInvalid)
+  end
+
+  it "採番できること" do
+    rule = NumberingRule.create!(:numbering_format => '%s', :current_number => 0)
+	rule.create_number.should == '1'
+	rule.current_number.should == 1
+  end
+
+  it "採番できること(ゼロ埋め)" do
+    rule = NumberingRule.create!(:numbering_format => '%5s', :current_number => 0)
+	rule.create_number.should == '00001'
+	rule.current_number.should == 1
+  end
+
+  it "採番できること（日付付き）" do
+    Time.stub!(:now).and_return(Time.local(2009, 2, 11))
+    rule = NumberingRule.create!(:numbering_format => 'EL%Y%m%d_%s', :current_number => 0)
+	rule.create_number.should == 'EL20090211_1'
+	rule.current_number.should == 1
   end
 end
