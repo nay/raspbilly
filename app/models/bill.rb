b8fc056258d6d00f68610ab162bb56eb5e0d49d7
nay <y.ohba@everyleaf.com>
2009-04-16 21:33:08 +0900

    make bill to calculate total_amount

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -5,5 +5,28 @@ class Bill < ActiveRecord::Base
 
   has_many :sale_items, :class_name => "BillItem::Sale"
   has_many :out_of_pocket_items, :class_name => "BillItem::OutOfPocket"
+  has_many :items, :class_name => "BillItem::Base" # 計算用
+
   accepts_nested_attributes_for :sale_items, :out_of_pocket_items, :allow_destroy => true, :reject_if => proc {|attributes| attributes['name'].blank? && attributes['amount'].blank? && attributes['description'].blank? }
+
+  after_save :update_total_amount_and_tax
+
+  private
+  def update_total_amount_and_tax
+    sum = 0
+    taxed_sum = 0
+    sale_items.each{|i|
+      return unless i.amount
+      sum += i.amount
+      taxed_sum += i.amount if i.taxed?
+    }
+    out_of_pocket_items.each{|i|
+      return unless i.amount
+      sum += i.amount
+      taxed_sum += i.amount if i.taxed?
+    }
+    self.tax = (taxed_sum * 1.05).ceil - taxed_sum # TODO: 計算方式の登録
+    self.total_amount = sum
+  end
+
 end
--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -21,4 +21,22 @@ describe Bill do
       b.save.should be_true
     end
   end
+  describe "save" do
+    describe "before_save update_total_amount_and_tax" do
+      before do
+        @bill = Bill.new(:customer_name => "test", :sale_items_attributes => [{:name => "item1", :formatted_amount => "120"}, {:name => "item2", :formatted_amount => "3,000"}])
+      end
+      it "売上２件の合計が正しいこと" do
+        @bill.save!
+        @bill.total_amount.should == 3120
+        @bill.tax.should == 156
+      end
+      it "立替が合計され、税金には反映されないこと" do
+        @bill.out_of_pocket_items.build(:name => "oop1", :formatted_amount => "5,000")
+        @bill.save!
+        @bill.total_amount.should == 8120
+        @bill.tax.should == 156
+      end
+    end
+  end
 end
