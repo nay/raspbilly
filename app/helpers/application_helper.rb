0efa7c578d7f6fcb2ca691a3bcc4e561c43449c2
nay <y.ohba@everyleaf.com>
2009-04-29 16:10:10 +0900

    modified show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def with_lack_attension(value, lack_attension)
+    return h(value) if value && (!value.kind_of?(String) || !value.blank?)
+    content_tag(:span, h(lack_attension), :class => "lackAttension")
+  end
 end
